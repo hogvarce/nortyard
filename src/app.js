230db9c8afd5bf43891bccb3f33386dbd19b171d
hogvarce <hogvarce@gmail.com>
2016-06-08 00:43:26 +0300

    добавление редактирования

--- src/app.js
+++ src/app.js
@@ -1,17 +1,30 @@
+const storage = $.localStorage;
 const App = {
     headers: [],
-    getHeaders: () => {
+    renderList: (tab) => {
+        let list = storage.get(tab) || [];
+        $('.table.active tbody').html('');
+        for (let tr in list) {
+            let html = '<tr>';
+            for (let td in list[tr].data) {
+                html += '<td>'+list[tr].data[td].value+'</td>';
+            }
+            html += '<td><button class="edit btn btn-primary">Редактировать</button> <button class="remove btn btn-danger">Удалить</button></td></tr>'
+            $('.table.active tbody').append(html);
+        }
+    },
+    getHeaders: (modal) => {
         App.headers = [];
         $('.table.active thead tr th').each(function(){
             App.headers.push($(this).text());
         });
-        $('.modal-body > form').html('');
+            $(modal).find('.modal-body > form').html('');
         for(let header in App.headers) {
-            $('.modal-body > form').append('<div class="form-group"><label for="'+App.headers[header]+'">'+App.headers[header]+'</label><input class="form-control" type="text" name="'+App.headers[header]+'"></div>');
+            $(modal).find('.modal-body > form').append('<div class="form-group"><label for="'+App.headers[header]+'">'+App.headers[header]+'</label><input class="form-control" type="text" name="'+App.headers[header]+'"></div>');
         }
     },
     writeData: () => {
-        let form = $('.modal-body > form');
+        let form = $('#myModal .modal-body > form');
         let data = [];
         let html = '<tr>';
         form.find('input').each(function(){
@@ -20,9 +33,53 @@ const App = {
         });
         html += '<td><button class="edit btn btn-primary">Редактировать</button> <button class="remove btn btn-danger">Удалить</button></td></tr>'
         $('.table.active tbody').append(html);
+        App.saveData(data);
+        $.jGrowl("Запись добавлена", {position: 'bottom-right'});
     },
     deleteData: (el) => {
-        $(el).parent().parent().remove();
+        if (confirm("Одумайся, глупец!.. Все-таки удалять?")){
+            let deleted = $(el).parent().parent();
+            let inx = deleted.index();
+            deleted.remove();
+            $.jGrowl("Запись удалена", {position: 'bottom-right'});
+            let header = $('#tabs .active a').text();
+            let list = storage.get(header) || [];
+            list.splice(inx, 1);
+            storage.set(header, list);
+        }
+    },
+    saveData: (data) => {
+        let header = $('#tabs .active a').text();
+        let list = storage.get(header) || [];
+        list.push({data});
+        storage.set(header, list);
+    },
+    itemEdit: '',
+    editData: (data) => {
+        let header = $('#tabs .active a').text();
+        App.itemEdit = $(data).parent().parent().index();
+        let list = storage.get(header) || [];
+        let item = list[App.itemEdit];
+        App.getHeaders('#editModal');
+        $('#editModal').modal('show');
+        $('#editModal .modal-body input').each(function(e){
+            $(this).val(item.data[e].value);
+        });
+    },
+    saveEditData: () => {
+        let data = [];
+        let header = $('#tabs .active a').text();
+        let form = $('#editModal .modal-body > form');
+        let html = '';
+        form.find('input').each(function(){
+            data.push({name: $(this).attr('name'), value: $(this).val()});
+            html += '<td>'+$(this).val()+'</td>'
+        });
+        html += '<td><button class="edit btn btn-primary">Редактировать</button> <button class="remove btn btn-danger">Удалить</button></td>'
+        let list = storage.get(header) || [];
+        list.splice(App.itemEdit, 1, {data});
+        storage.set(header, list);
+        $('.table.active tbody tr').eq(App.itemEdit).html(html);
     }
 };
 
--- src/index.js
+++ src/index.js
@@ -1,20 +1,35 @@
 import App from './app.js';
 
 $(document).ready(() => {
+
+    $.jGrowl("Hello!", {position: 'bottom-right'});
+
+    App.renderList($('#tabs .active a').text());
+
     $('#tabs a').click((e) => {
       e.preventDefault();
       $(this).tab('show');
     });
 
+    $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {;
+         App.renderList($('#tabs .active a').text());
+    });
+
     $('#myModal').on('show.bs.modal',(e) => {
-        App.getHeaders();
+        App.getHeaders('#myModal');
     });
 
     $('#getWrite').on('click', () => {
         App.writeData();
     })
 
+    $('#editWrite').on('click', () => {
+        App.saveEditData();
+    })
+
     $(document).on('click', 'button.remove', (e) => {
         App.deleteData(e.target);
-    })
+    }).on('click', 'button.edit', (e) => {
+        App.editData(e.target);
+    });
 });

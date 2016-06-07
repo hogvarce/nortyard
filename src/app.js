63e102884e4b4372957d18cd8250a2487773858e
hogvarce <hogvarce@gmail.com>
2016-06-07 21:27:01 +0300

    Добавление записей по клику в модальном окне

--- src/app.js
+++ src/app.js
@@ -1,14 +1,28 @@
 const App = {
     headers: [],
-    getData: () => {
+    getHeaders: () => {
         App.headers = [];
         $('.table.active thead tr th').each(function(){
             App.headers.push($(this).text());
         });
         $('.modal-body > form').html('');
         for(let header in App.headers) {
-            $('.modal-body > form').append('<label for="'+App.headers[header]+'">'+App.headers[header]+'</label><input type="text" name="'+App.headers[header]+'"><br>');
+            $('.modal-body > form').append('<div class="form-group"><label for="'+App.headers[header]+'">'+App.headers[header]+'</label><input class="form-control" type="text" name="'+App.headers[header]+'"></div>');
         }
+    },
+    writeData: () => {
+        let form = $('.modal-body > form');
+        let data = [];
+        let html = '<tr>';
+        form.find('input').each(function(){
+            data.push({name: $(this).attr('name'), value: $(this).val()});
+            html += '<td>'+$(this).val()+'</td>'
+        });
+        html += '<td><button class="edit btn btn-primary">Редактировать</button> <button class="remove btn btn-danger">Удалить</button></td></tr>'
+        $('.table.active tbody').append(html);
+    },
+    deleteData: (el) => {
+        $(el).parent().parent().remove();
     }
 };
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import App from './app.js';
 
-$(document).ready(function(){
-    $('#tabs a').click(function (e) {
+$(document).ready(() => {
+    $('#tabs a').click((e) => {
       e.preventDefault();
       $(this).tab('show');
     });
 
-    $('#myModal').on('show.bs.modal', function (e) {
-        App.getData();
+    $('#myModal').on('show.bs.modal',(e) => {
+        App.getHeaders();
     });
+
+    $('#getWrite').on('click', () => {
+        App.writeData();
+    })
+
+    $(document).on('click', 'button.remove', (e) => {
+        App.deleteData(e.target);
+    })
 });
